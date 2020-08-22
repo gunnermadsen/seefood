89d9708053c99e2825e364c409ca6dbfa1057834
gunnermadsen <gunner.madsen@outlook.com>
2020-08-22 08:40:46 -0700

    Updating navigation title based on the status of the identifier

--- SeeFood/ViewController.swift
+++ SeeFood/ViewController.swift
@@ -46,7 +46,15 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
             guard let results = request.results as? [VNClassificationObservation] else {
                 fatalError("Model failed to process image")
             }
-            print(results)
+            
+            if let firstResult = results.first {
+                if firstResult.identifier.contains("hotdog") {
+                    self.navigationItem.title = "Hotdog!"
+                }
+                else {
+                    self.navigationItem.title = "Not HotDog!"
+                }
+            }
         }
         
         let handler = VNImageRequestHandler(ciImage: image)
